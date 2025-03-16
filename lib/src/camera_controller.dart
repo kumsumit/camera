1637457aeb043829ae9e28f6ea058c92873790f8
sumitsharansatsangi <sharansumitkumar@gmail.com>
2025-03-16 18:26:43 +0530

    code updates

--- lib/src/camera_controller.dart
+++ lib/src/camera_controller.dart
@@ -480,14 +480,11 @@ class CameraController extends ValueNotifier<CameraValue> {
   /// Throws a [CameraException] if image streaming or video recording has
   /// already started.
   ///
-  /// The `startImageStream` method is only available on Android and iOS (other
-  /// platforms won't be supported in current setup).
+  /// The `startImageStream` method is only available on platforms that
+  /// report support for image streaming via [supportsImageStreaming].
   ///
   // TODO(bmparr): Add settings for resolution and fps.
   Future<void> startImageStream(onLatestImageAvailable onAvailable) async {
-    assert(defaultTargetPlatform == TargetPlatform.android ||
-        defaultTargetPlatform == TargetPlatform.iOS);
-    _throwIfNotInitialized('startImageStream');
     if (value.isRecordingVideo) {
       throw CameraException(
         'A video recording is already started.',
@@ -518,12 +515,9 @@ class CameraController extends ValueNotifier<CameraValue> {
   /// Throws a [CameraException] if image streaming was not started or video
   /// recording was started.
   ///
-  /// The `stopImageStream` method is only available on Android and iOS (other
-  /// platforms won't be supported in current setup).
+  /// The `stopImageStream` method is only available on platforms that
+  /// report support for image streaming via [supportsImageStreaming].
   Future<void> stopImageStream() async {
-    assert(defaultTargetPlatform == TargetPlatform.android ||
-        defaultTargetPlatform == TargetPlatform.iOS);
-    _throwIfNotInitialized('stopImageStream');
     if (!value.isStreamingImages) {
       throw CameraException(
         'No camera is streaming images',
@@ -870,6 +864,7 @@ class CameraController extends ValueNotifier<CameraValue> {
       throw CameraException(e.code, e.message);
     }
   }
+
 
   /// Releases the resources of this camera.
   @override
